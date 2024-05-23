c77a6c3b3fc81e0239c0577e327891e800731664
nyxb <dennis.ollhoff@joypix.gg>
2024-05-23 18:52:52 +0200

    🛠️ build(build.config.ts): add build configuration for project automation
    📦 packaging(bun/package.json): introduce package.json for bun to specify package manager

---
+++ build.config.ts
@@ -0,0 +1,18 @@
+import { basename } from 'node:path'
+import { defineBuildConfig } from 'unbuild'
+import fg from 'fast-glob'
+
+export default defineBuildConfig({
+   entries: [
+      ...fg.sync('src/commands/*.ts').map(i => ({
+         input: i.slice(0, -3),
+         name: basename(i).slice(0, -3),
+      })),
+   ],
+   clean: true,
+   declaration: true,
+   rollup: {
+      emitCJS: true,
+      inlineDependencies: true,
+   },
+})
